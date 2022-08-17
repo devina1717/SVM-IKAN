629c60a1d5ab1fa9374d6d1f84d3bb93c784ff2f
devina1717 <111436849+devina1717@users.noreply.github.com>
2022-08-17 21:12:56 +0700

    Update ikan.py

--- ikan.py
+++ ikan.py
@@ -77,7 +77,7 @@ choice = st.sidebar.selectbox('Login', ['Admin', 'User'])
 # Path
 path_grafik = r"./Img/graph.jpg"
 path_ikan_jpg = r"./Img/ket_ikan.jpg"
-path_model = r"./300px.p" #Edit / ganti file model
+path_model = r".SVM-IKAN/300px.p" #Edit / ganti file model
 
 
 # App
